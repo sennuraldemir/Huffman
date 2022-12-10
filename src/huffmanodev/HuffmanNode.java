b54bf5b0c22f7c44f7e04f83b70415b6429e9df7
Sennur Aldemir <99283497+sennuraldemir@users.noreply.github.com>
2022-12-10 21:45:26 +0300

    Update HuffmanNode.java

--- src/huffmanodev/HuffmanNode.java
+++ src/huffmanodev/HuffmanNode.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package huffmanodev;
 
 import java.util.Comparator;
@@ -15,10 +12,7 @@ class HuffmanNode {
     HuffmanNode right;
 }
 
-// comparator class helps to compare the node
-// on the basis of one of its attribute.
-// Here we will be compared
-// on the basis of data values of the nodes.
+
 class MyComparator implements Comparator<HuffmanNode> {
 
     public int compare(HuffmanNode x, HuffmanNode y) {
