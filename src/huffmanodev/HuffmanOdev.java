1b2b5707a978bfdfac548f71e70c807d43fb6595
Sennur Aldemir <99283497+sennuraldemir@users.noreply.github.com>
2022-12-10 21:47:07 +0300

    Update HuffmanOdev.java

--- src/huffmanodev/HuffmanOdev.java
+++ src/huffmanodev/HuffmanOdev.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package huffmanodev;
 
 import static huffmanodev.Huffman.printCode;
@@ -14,9 +11,7 @@ import java.util.Scanner;
  */
 public class HuffmanOdev {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
@@ -26,15 +21,12 @@ public class HuffmanOdev {
         char[] charArray = {'a', 'b', 'c', 'd', 'e', 'f'};
         int[] charfreq = {5, 9, 12, 13, 16, 45};
 
-        // creating a priority queue q.
-        // makes a min-priority queue(min-heap).
         PriorityQueue<HuffmanNode> q
                 = new PriorityQueue<HuffmanNode>(n, new MyComparator());
 
         for (int i = 0; i < n; i++) {
 
-            // creating a Huffman node object
-            // and add it to the priority queue.
+          
             HuffmanNode hn = new HuffmanNode();
 
             hn.c = charArray[i];
@@ -43,54 +35,44 @@ public class HuffmanOdev {
             hn.left = null;
             hn.right = null;
 
-            // add functions adds
-            // the huffman node to the queue.
+           
             q.add(hn);
         }
 
-        // create a root node
+       
         HuffmanNode root = null;
 
-        // Here we will extract the two minimum value
-        // from the heap each time until
-        // its size reduces to 1, extract until
-        // all the nodes are extracted.
+      
         while (q.size() > 1) {
 
-            // first min extract.
+            
             HuffmanNode x = q.peek();
             q.poll();
 
-            // second min extract.
+            
             HuffmanNode y = q.peek();
             q.poll();
 
-            // new node f which is equal
+           
             HuffmanNode f = new HuffmanNode();
 
-            // to the sum of the frequency of the two nodes
-            // assigning values to the f node.
             f.data = x.data + y.data;
             f.c = '-';
 
-            // first extracted node as left child.
+       
             f.left = x;
 
-            // second extracted node as the right child.
             f.right = y;
 
-            // marking the f node as the root node.
             root = f;
 
-            // add this node to the priority-queue.
+           
             q.add(f);
         }
 
-        // print the codes by traversing the tree
         printCode(root, "");
     }
 }
 
-// node class is the basic structure
-// of each node present in the Huffman - tree.
+
 
